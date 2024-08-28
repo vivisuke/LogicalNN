91a98805b4428b154ff4eeb0d6759998b835b7f8
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2024-08-29 06:00:34 +0900

    0025 ◎ C 08/29 08/29 0.001│ToDo│GraphRect3i：x1, x2, x3 座標軸表示

--- logicalnn/scripts/graph_rect_3i.gd
+++ logicalnn/scripts/graph_rect_3i.gd
@@ -1,5 +1,24 @@
 extends ColorRect
 
+var SZ = self.size
+var SCREEN_WD = SZ.x
+var SCREEN_HT = SZ.y
+const SPACE_TOP = 30.0
+const SPACE_BOTTOM = 30.0
+const SPACE_LEFT = 40.0
+var GRAPH_HT = SCREEN_HT - (SPACE_TOP + SPACE_BOTTOM)
+var GRAPH_WD = GRAPH_HT
+const SCALE_WD = 6				# 目盛り幅
+const LLT = 8
+const LT = SPACE_LEFT
+var RT = SPACE_LEFT + GRAPH_WD
+const TOP = SPACE_TOP
+var BTM = SPACE_TOP + GRAPH_HT
+const BTM_OFST = 12
+const LT_OFST = 12
+var ORG_X = SPACE_LEFT + GRAPH_WD/2
+var ORG_Y = SPACE_TOP + GRAPH_HT/2
+const DOT_RADIUS = 4.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,3 +38,7 @@ func _draw():
 	style_box.shadow_size = 8                   # 影（ぼかし）サイズ
 	style_box.shadow_color = Color.GRAY
 	draw_style_box(style_box, Rect2(Vector2(0, 0), self.size))      # style_box に設定した矩形を描画
+	# 座標軸描画
+	draw_line(Vector2(LT, ORG_Y-GRAPH_HT/4), Vector2(RT, ORG_Y+GRAPH_HT/4), Color.GRAY)		# x1 軸
+	draw_line(Vector2(LT, ORG_Y+GRAPH_HT/4), Vector2(RT, ORG_Y-GRAPH_HT/4), Color.GRAY)		# x2 軸
+	draw_line(Vector2(ORG_X, TOP), Vector2(ORG_X, BTM), Color.GRAY)		# x3軸
