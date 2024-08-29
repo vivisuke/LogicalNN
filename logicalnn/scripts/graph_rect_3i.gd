e13d4853a7403778ccb6370dd1ad91963a97dea8
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2024-08-29 10:37:40 +0900

    0027 ◎ C 08/29 08/29 0.001│ToDo│GraphRect3i：4x4x4 サイズ立方体描画してみる 0028 ◎ C 08/29 08/29 0.001│ToDo│GraphRect3i：(x1, x2, x3) -> (x, y) 座標変換関数実装

--- logicalnn/scripts/graph_rect_3i.gd
+++ logicalnn/scripts/graph_rect_3i.gd
@@ -19,14 +19,56 @@ const LT_OFST = 12
 var ORG_X = SPACE_LEFT + GRAPH_WD/2
 var ORG_Y = SPACE_TOP + GRAPH_HT/2
 const DOT_RADIUS = 4.0
+var angle_3d = PI/6.0		# 30°
+#var angle_3d = PI/4.0		# 45°
+var angle30 = PI / 6.0
+var scale_3d2d = 50.0
 
-# Called when the node enters the scene tree for the first time.
+func trans_3d_2d(x1:float, x2:float, x3:float) -> Vector2:
+	if true:
+		var x = -cos(angle_3d) * x1
+		var y = sin(angle_3d) * x1
+		var angle2 = angle_3d + PI/2.0
+		x -= x2 * cos(angle2)
+		y += x2 * sin(angle2)
+		y -= x3
+		return Vector2(x*scale_3d2d+ORG_X, y*scale_3d2d/2.0+ORG_Y)
+	else:
+		var cos30 = cos(angle30)
+		var sin30 = sin(angle30)
+		var x = 0
+		var y = 0
+		x -= x1 * cos30
+		y += x1 * sin30
+		x += x2 * cos30
+		y += x2 * sin30
+		y -= x3
+		##return Vector2(x, y)
+		return Vector2(x*scale_3d2d+ORG_X, y*scale_3d2d+ORG_Y)
+
+# 目盛り値ラベル設置
+func add_axis_label(pos, txt):
+	var lbl = Label.new()
+	lbl.add_theme_color_override("font_color", Color.BLACK)
+	lbl.text = txt
+	lbl.position = pos
+	add_child(lbl)
+	return lbl
+
 func _ready():
+	# "x1", "x2", "x3" 表示
+	add_axis_label(Vector2(LT+BTM_OFST, ORG_Y+GRAPH_HT/4), "x1")
+	add_axis_label(Vector2(RT-BTM_OFST*2, ORG_Y+GRAPH_HT/4), "x2")
+	add_axis_label(Vector2(ORG_X+4, TOP), "x3")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	angle_3d += PI/360		# 1/2°
+	#angle_3d += PI/180		# 1°
+	if angle_3d >= 2*PI: angle_3d -= 2*PI
+	queue_redraw()
 	pass
 
 func _draw():
@@ -39,6 +81,35 @@ func _draw():
 	style_box.shadow_color = Color.GRAY
 	draw_style_box(style_box, Rect2(Vector2(0, 0), self.size))      # style_box に設定した矩形を描画
 	# 座標軸描画
-	draw_line(Vector2(LT, ORG_Y-GRAPH_HT/4), Vector2(RT, ORG_Y+GRAPH_HT/4), Color.GRAY)		# x1 軸
-	draw_line(Vector2(LT, ORG_Y+GRAPH_HT/4), Vector2(RT, ORG_Y-GRAPH_HT/4), Color.GRAY)		# x2 軸
-	draw_line(Vector2(ORG_X, TOP), Vector2(ORG_X, BTM), Color.GRAY)		# x3軸
+	draw_line(trans_3d_2d(6, 0, 0), trans_3d_2d(-6, 0, 0), Color.GRAY)		# x1 軸
+	draw_line(trans_3d_2d(0, 6, 0), trans_3d_2d(0, -6, 0), Color.GRAY)		# x2 軸
+	draw_line(trans_3d_2d(0, 0, 6), trans_3d_2d(0, 0, -6), Color.GRAY)		# x3 軸
+	#
+	#draw_circle(trans_3d_2d(0, 0, 0), DOT_RADIUS, Color.BLACK)		# 原点
+	#draw_circle(trans_3d_2d(2, 0, 0), DOT_RADIUS, Color.BLACK)		# x1 = 2.0
+	#draw_circle(trans_3d_2d(0, 2, 0), DOT_RADIUS, Color.BLACK)		# x2 = 2.0
+	#draw_circle(trans_3d_2d(0, 0, 2), DOT_RADIUS, Color.BLACK)
+	if true:
+		# 4x4x4 立方体頂点
+		draw_circle(trans_3d_2d( 2,  2,  2), DOT_RADIUS, Color.BLACK)
+		draw_circle(trans_3d_2d(-2,  2,  2), DOT_RADIUS, Color.BLACK)
+		draw_circle(trans_3d_2d( 2, -2,  2), DOT_RADIUS, Color.BLACK)
+		draw_circle(trans_3d_2d(-2, -2,  2), DOT_RADIUS, Color.BLACK)
+		draw_circle(trans_3d_2d( 2,  2, -2), DOT_RADIUS, Color.BLACK)
+		draw_circle(trans_3d_2d(-2,  2, -2), DOT_RADIUS, Color.BLACK)
+		draw_circle(trans_3d_2d( 2, -2, -2), DOT_RADIUS, Color.BLACK)
+		draw_circle(trans_3d_2d(-2, -2, -2), DOT_RADIUS, Color.BLACK)
+	if true:
+		# 4x4x4 立方体エッジ
+		draw_dashed_line(trans_3d_2d( 2,  2,  2), trans_3d_2d( -2,  2,  2), Color.BLACK, 2.0)
+		draw_dashed_line(trans_3d_2d(-2,  2,  2), trans_3d_2d( -2, -2,  2), Color.BLACK, 2.0)
+		draw_dashed_line(trans_3d_2d(-2, -2,  2), trans_3d_2d(  2, -2,  2), Color.BLACK, 2.0)
+		draw_dashed_line(trans_3d_2d( 2, -2,  2), trans_3d_2d(  2,  2,  2), Color.BLACK, 2.0)
+		draw_dashed_line(trans_3d_2d( 2,  2, -2), trans_3d_2d( -2,  2, -2), Color.BLACK, 2.0)
+		draw_dashed_line(trans_3d_2d(-2,  2, -2), trans_3d_2d( -2, -2, -2), Color.BLACK, 2.0)
+		draw_dashed_line(trans_3d_2d(-2, -2, -2), trans_3d_2d(  2, -2, -2), Color.BLACK, 2.0)
+		draw_dashed_line(trans_3d_2d( 2, -2, -2), trans_3d_2d(  2,  2, -2), Color.BLACK, 2.0)
+		draw_dashed_line(trans_3d_2d( 2,  2,  2), trans_3d_2d(  2,  2, -2), Color.BLACK, 2.0)
+		draw_dashed_line(trans_3d_2d(-2,  2,  2), trans_3d_2d( -2,  2, -2), Color.BLACK, 2.0)
+		draw_dashed_line(trans_3d_2d(-2, -2,  2), trans_3d_2d( -2, -2, -2), Color.BLACK, 2.0)
+		draw_dashed_line(trans_3d_2d( 2, -2,  2), trans_3d_2d(  2, -2, -2), Color.BLACK, 2.0)
